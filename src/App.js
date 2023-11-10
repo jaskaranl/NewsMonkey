54b16bf072dbd7cddffa1a52603179fb119b444a
Jaskaran Kamboj <kambojjaskaran321@gmail.com>
2023-11-10 19:04:40 +0530

    updated liks

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import React, { Component } from "react";
 import Navbar from "./components/Navbar";
 import News from "./components/News";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <Router>
+        <HashRouter>
           <Navbar />
 
           <Switch>
@@ -68,7 +69,7 @@ export default class App extends Component {
               />
             </Route>
           </Switch>
-        </Router>
+        </HashRouter>
       </div>
     );
   }
